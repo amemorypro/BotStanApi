01f3a0597e21a881f9a5d8a5a86e5d6f851f827f
boto.agency - Develop IT Solution, chat-bots and CRM systems <42206780+amemorypro@users.noreply.github.com>
2023-12-15 14:15:17 +0200

    Update Curl.php

--- src/base/Curl.php
+++ src/base/Curl.php
@@ -106,7 +106,7 @@ class Curl
     {
         $options = $this->_options;
         $default = $this->_default_options;
-        return options + $default;
+        return $options + $default;
     }
 
     /**
